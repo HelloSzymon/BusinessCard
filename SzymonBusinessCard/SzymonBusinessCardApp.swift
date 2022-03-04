06c7af2608ab28175b601d0d083d43009b8d7588
Szymon Wnuk <szwnuk1@gmail.com>
2022-03-04 19:18:09 +0100

    Initial Commit

---
+++ SzymonBusinessCard/SzymonBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SzymonBusinessCardApp.swift
+//  SzymonBusinessCard
+//
+//  Created by Szymon Wnuk on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SzymonBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
